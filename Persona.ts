91b01621aaf22a3375fe3b670286874f566c9474
maiaaaam <112764486+maiaaaam@users.noreply.github.com>
2023-03-26 16:03:23 +0200

    Update Persona.ts

--- Persona.ts
+++ Persona.ts
@@ -50,12 +50,11 @@ class Person {
   }
 }
 
-// Create some Person objects
+
 const person1 = new Person(1, 'John Doe', new Mail('john.doe@example.com'), new Address('123 Main St', 'Anytown', '12345', 'USA'), new PhoneNumber('555-123-4567'));
 const person2 = new Person(2, 'Jane Smith', new Mail('jane.smith@example.com'), new Address('456 Oak St', 'Sometown', '54321', 'USA'), new PhoneNumber('555-987-6543'));
 const person3 = new Person(3, 'Bob Johnson', new Mail('bob.johnson@example.com'), new Address('789 Elm St', 'Otherplace', '67890', 'USA'), new PhoneNumber('555-555-5555'));
 
-// Modify the information for person1 by searching by ID
 const targetId = 1;
 
 if (person1.getId() === targetId) {
@@ -68,7 +67,6 @@ if (person1.getId() === targetId) {
   person1.setPhone(newPhone);
 }
 
-// Log the updated information for all Person objects
 console.log(person1.getId(), person1.getName(), person1.getEmail().getEmail(), person1.getAddress().getAddress(), person1.getPhone().getPhoneNumber());
 console.log(person2.getId(), person2.getName(), person2.getEmail().getEmail(), person2.getAddress().getAddress(), person2.getPhone().getPhoneNumber());
 console.log(person3.getId(), person3.getName(), person3.getEmail().getEmail(), person3.getAddress().getAddress(), person3.getPhone().getPhoneNumber());
